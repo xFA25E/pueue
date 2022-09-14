6fa8d5f873afda2f7025bfc0be45ad11605c0aa1
xFA25E <55886566+xFA25E@users.noreply.github.com>
2022-09-14 23:29:56 +0200

    Update flake

--- flake.nix
+++ flake.nix
@@ -1,36 +1,54 @@
 {
-  inputs = {
-    eldev = { url = "github:doublep/eldev/1.2"; flake = false; };
+  inputs.eldev = {
+    url = "github:doublep/eldev/1.2.2";
+    flake = false;
   };
-  outputs = { self, nixpkgs, eldev }: let
-    name = "pueue";
+  outputs = {
+    self,
+    nixpkgs,
+    emacs-overlay,
+    eldev,
+  }: let
     system = "x86_64-linux";
-    pkgs = import nixpkgs { inherit system; overlays = [ self.overlays.eldev ]; };
+    pkgs = import nixpkgs {
+      inherit system;
+      overlays = [self.overlays.eldev];
+    };
 
-    inherit (builtins) elemAt match readFile;
+    inherit (builtins) attrNames elemAt foldl' head map match readDir readFile;
+    inherit (builtins) stringLength tail;
+    inherit (pkgs.lib.lists) filter;
+    inherit (pkgs.lib.strings) hasSuffix removeSuffix;
+    parse = pkgs.callPackage "${emacs-overlay}/parse.nix" {};
+
+    names = filter (hasSuffix ".el") (attrNames (readDir self));
+    name = removeSuffix ".el" (foldl' (acc: elm:
+      if (stringLength elm) < (stringLength acc)
+      then elm
+      else acc) (head names) (tail names));
     mainFile = readFile "${self}/${name}.el";
+
     version = elemAt (match ".*\n;; Version: ([^\n]+).*" mainFile) 0;
     url = elemAt (match ".*\n;; URL: ([^\n]+).*" mainFile) 0;
+    deps = parse.parsePackagesFromPackageRequires mainFile;
   in {
-
     overlays = {
-      default = self.overlays.${name};
-
-      ${name} = final: prev: {
-        emacsPackagesFor = emacs: (prev.emacsPackagesFor emacs).overrideScope' (
-          efinal: eprev: {
-            ${name} = efinal.melpaBuild {
-              inherit version;
-              pname = name;
-              src = self;
-              commit = self.rev;
-              recipe = final.writeText "recipe" ''
-                (${name} :fetcher git :url "${url}")
-              '';
-              packageRequires = with efinal; [ with-editor ];
-            };
-          }
-        );
+      default = final: prev: {
+        emacsPackagesFor = emacs:
+          (prev.emacsPackagesFor emacs).overrideScope' (
+            efinal: eprev: {
+              ${name} = efinal.melpaBuild {
+                inherit version;
+                pname = name;
+                src = self;
+                commit = self.rev;
+                recipe = final.writeText "recipe" ''
+                  (${name} :fetcher git :url "${url}")
+                '';
+                packageRequires = map (dep: efinal.${dep}) deps;
+              };
+            }
+          );
       };
 
       eldev = final: prev: {
@@ -41,23 +59,21 @@
           dontPatch = true;
           dontConfigure = true;
           dontBuild = true;
-          nativeBuildInputs = [ final.emacs ];
+          nativeBuildInputs = [final.emacs];
           installPhase = ''
             mkdir -p $out/bin
             cp $src/bin/eldev $out/bin/
           '';
         };
       };
-
     };
 
     devShells.${system}.default = pkgs.mkShell {
       inherit name;
-      buildInputs = [ pkgs.eldev ];
+      buildInputs = [pkgs.eldev];
       shellHook = ''
         export ELDEV_DIR=$PWD/.eldev
       '';
     };
-
   };
 }
